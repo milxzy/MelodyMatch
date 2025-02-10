8b6f03b45c990e5caf9a46ebecdbadcd16531e43
miles <miles@miless-MacBook-Air.local>
2025-02-09 23:10:00 -0600

    client test setup

---
+++ client/src/tests/setup.js
@@ -0,0 +1,12 @@
+import { expect, afterEach } from 'vitest';
+import { cleanup } from '@testing-library/react';
+import '@testing-library/jest-dom';
+
+// Cleanup after each test
+afterEach(() => {
+  cleanup();
+});
+
+// Mock environment variables
+process.env.VITE_API_URL = 'http://localhost:4000';
+process.env.VITE_SPOTIFY_CLIENT_ID = 'test_client_id';

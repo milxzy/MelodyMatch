49a92821fbaf3569049a855b91f345e6e1164583
Miles Collins <38504704+milxzy@users.noreply.github.com>
2026-01-15 12:01:39 -0600

    fixed typos

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -24,7 +24,7 @@ import {
   VStack,
 } from "@chakra-ui/react";
 
-const API_URL = import.meta.env.VITE_API_URL;
+const API_URL = import.meta.env.VITE_API_URL || 'https://melodymatch-3ro0.onrender.com';
 
 const Welcome = () => {
   const navigate = useNavigate();
